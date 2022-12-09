549979a5d9744dcec969bab713fa62dc9268dc76
CorentinTrt <corentintruffaut@gmail.com>
2022-12-09 05:49:13 +0100

    constants/players.ts - add player's data

---
+++ src/constants/players.ts
@@ -0,0 +1,92 @@
+type Player = {
+	id: String;
+	name: String;
+	authCode: String;
+};
+
+const players: Player[] = [
+	{
+		id: '1',
+		name: 'John',
+		authCode: 'qw',
+	},
+	{
+		id: '2',
+		name: 'Caroline',
+		authCode: 'as',
+	},
+	{
+		id: '3',
+		name: 'Lisy',
+		authCode: 'zx',
+	},
+	{
+		id: '4',
+		name: 'Sara',
+		authCode: 'er',
+	},
+	{
+		id: '5',
+		name: 'Alexandre',
+		authCode: 'df',
+	},
+	{
+		id: '6',
+		name: 'Alexia',
+		authCode: 'cv',
+	},
+	{
+		id: '7',
+		name: 'Patrick',
+		authCode: 'ty',
+	},
+	{
+		id: '8',
+		name: 'Nicolas',
+		authCode: 'gh',
+	},
+	{
+		id: '9',
+		name: 'Johan',
+		authCode: 'bn',
+	},
+	{
+		id: '10',
+		name: 'Sasha',
+		authCode: 'ui',
+	},
+	{
+		id: '11',
+		name: 'Chris',
+		authCode: 'jk',
+	},
+	{
+		id: '12',
+		name: 'Emma',
+		authCode: 'm,',
+	},
+	{
+		id: '13',
+		name: 'Jessica',
+		authCode: 'op',
+	},
+	{
+		id: '14',
+		name: 'Pedro',
+		authCode: 'l;',
+	},
+	{
+		id: '15',
+		name: 'Ana',
+		authCode: './',
+	},
+	{
+		id: '16',
+		name: 'Hugo',
+		authCode: '[]',
+	},
+];
+
+export type { Player };
+
+export default players;
