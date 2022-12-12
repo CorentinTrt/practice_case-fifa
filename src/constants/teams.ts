ab8c2b0aac24f0a102d6d8cf233cd69847055444
CorentinTrt <corentintruffaut@gmail.com>
2022-12-12 09:01:18 +0100

    constants/teams.ts - fix types + add labels custom

--- src/constants/teams.ts
+++ src/constants/teams.ts
@@ -1,19 +1,19 @@
 type Team = {
-	id: String;
-	label: String;
-	idPlayerA: String;
-	idPlayerB: String;
+	id: string;
+	label: string;
+	idPlayerA: string;
+	idPlayerB: string;
 };
 
 const teams: Team[] = [
-	{ id: '1', label: 'Team A', idPlayerA: '1', idPlayerB: '2' },
-	{ id: '2', label: 'Team B', idPlayerA: '3', idPlayerB: '4' },
-	{ id: '3', label: 'Team C', idPlayerA: '5', idPlayerB: '6' },
-	{ id: '4', label: 'Team D', idPlayerA: '7', idPlayerB: '8' },
-	{ id: '5', label: 'Team E', idPlayerA: '9', idPlayerB: '10' },
-	{ id: '6', label: 'Team F', idPlayerA: '11', idPlayerB: '12' },
-	{ id: '7', label: 'Team G', idPlayerA: '13', idPlayerB: '14' },
-	{ id: '8', label: 'Team H', idPlayerA: '15', idPlayerB: '16' },
+	{ id: '1', label: 'Swimming Unicorns', idPlayerA: '1', idPlayerB: '2' },
+	{ id: '2', label: 'Flying Beavers', idPlayerA: '3', idPlayerB: '4' },
+	{ id: '3', label: 'Crying Mooses', idPlayerA: '5', idPlayerB: '6' },
+	{ id: '4', label: 'Shouting Frogs', idPlayerA: '7', idPlayerB: '8' },
+	{ id: '5', label: 'Diving Chikens', idPlayerA: '9', idPlayerB: '10' },
+	{ id: '6', label: 'Jumping Sloths', idPlayerA: '11', idPlayerB: '12' },
+	{ id: '7', label: 'Running Dolphins', idPlayerA: '13', idPlayerB: '14' },
+	{ id: '8', label: 'Dancing Worms', idPlayerA: '15', idPlayerB: '16' },
 ];
 
 export type { Team };
