05f9c40cf7b707b4dd32f25ec5a31b8b18c9d63e
CorentinTrt <corentintruffaut@gmail.com>
2022-12-12 09:03:30 +0100

    _app.tsx - add fonts imports + layout

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,28 @@
-import '../styles/globals.scss';
+import Head from 'next/head';
 import type { AppProps } from 'next/app';
+import '../styles/globals.scss';
+
+import Layout from '@sections/Layout';
 
 export default function App({ Component, pageProps }: AppProps) {
-	return <Component {...pageProps} />;
+	return (
+		<>
+			<Head>
+				<title>Home App</title>
+				<link rel='preconnect' href='https://fonts.googleapis.com' />
+				<link
+					rel='preconnect'
+					href='https://fonts.gstatic.com'
+					crossOrigin='true'
+				/>
+				<link
+					href='https://fonts.googleapis.com/css2?family=Montserrat:wght@200;400;600;700&display=swap'
+					rel='stylesheet'
+				/>
+			</Head>
+			<Layout>
+				<Component {...pageProps} />;
+			</Layout>
+		</>
+	);
 }
